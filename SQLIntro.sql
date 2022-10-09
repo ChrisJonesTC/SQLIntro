26f24ccd11efce05a64a0b6b47de560cbc108a25
ChrisJonesTC <cfjones91@gmail.com>
2022-10-09 12:30:15 -0400

    SQL Joins

--- SQLIntro.sql
+++ SQLIntro.sql
@@ -1,39 +1,48 @@
--- find all products
-SELECT * FROM products;
--- find all products that cost $1400
-SELECT * FROM products
-WHERE price = 1400;
--- find all products that cost $11.99 or $13.99
-SELECT * FROM products
-WHERE price = 11.99 OR price = 13.99; 
--- find all products that do NOT cost 11.99 - using NOT
-SELECT * FROM products
-WHERE NOT price = 11.99;
--- find all products and sort them by price from greatest to least
-SELECT price FROM products
-ORDER BY price desc;
--- find all employees who don't have a middle initial
-SELECT * FROM employees
-WHERE MiddleInitial is null;
--- find distinct product prices
-SELECT distinct(price) FROM products;
--- find all employees whose first name starts with the letter ‘j’
-SELECT * FROM employees
-WHERE FirstName LIKE 'j%';
--- find all Macbooks
-SELECT * FROM products
-WHERE Name LIKE '%macbook%';
--- find all products that are on sale
-SELECT * FROM products
-WHERE OnSale = 1;
--- find the average price of all products
-SELECT avg(Price)
-FROM products;
--- find all Geek Squad employees who don't have a middle initial
-SELECT * FROM employees
-WHERE title = "Geek Squad" AND MiddleInitial IS Null;
--- find all products from the products table whose stock level is in the range of 500 to 1200.
--- Order by Price from least to greatest. Hint: Use the between keyword
-SELECT * FROM products
-WHERE Stocklevel BETWEEN 500 AND 1200
-ORDER BY price asc;
+/* joins: select all the computers from the products table:
+using the products table and the categories table, return the product name and the category name */
+ select p.name, c.name
+ from products AS p
+ inner join categories AS c on c.CategoryID = p.CategoryID
+ where c.name = "computers";
+
+/* joins: find all product names, product prices, and products ratings that have a rating of 5 */
+ SELECT p.Name, p.Price, r.Rating
+ FROM products AS p
+ INNER JOIN reviews AS r ON r.ProductID = p.ProductID
+ WHERE r.Rating = 5;
+ 
+/* joins: find the employee with the most total quantity sold.  use the sum() function and group by */
+SELECT e.FirstName, e.LastName, sum(s.Quantity) AS total
+FROM sales AS s
+INNER JOIN employees AS e ON e.EmployeeID = s.EmployeeID
+GROUP BY e.EmployeeID
+ORDER BY Total DESC
+LIMIT 2;
+
+/* joins: find the name of the department, and the name of the category for Appliances and Games */
+SELECT d.Name AS "Department Name", c.Name AS "Category Name"
+FROM departments AS d
+INNER JOIN categories AS c ON c.DepartmentID = d.DepartmentID
+WHERE c.Name = "Appliances" OR c.Name = "Games";
+
+/* joins: find the product name, total # sold, and total price sold,
+ for Eagles: Hotel California --You may need to use SUM() */
+SELECT p.Name, sum(s.Quantity) AS "Total Sold", sum(s.Quantity * s.PricePerUnit) AS "Total Price"
+FROM products AS p
+INNER JOIN sales AS s ON s.ProductID = p.ProductID
+WHERE p.ProductID = 97;
+SELECT * FROM sales WHERE ProductID = 97;
+
+/* joins: find Product name, reviewer name, rating, and comment on the Visio TV. (only return for the lowest rating!) */
+SELECT p.Name, r.Reviewer, r.Rating, r.Comment
+FROM products AS p
+INNER JOIN reviews AS r ON r.ProductID = p.ProductID
+WHERE p.ProductID = 857 AND r.Rating = 1;
+
+-- ------------------------------------------ Extra - May be difficult
+/* Your goal is to write a query that serves as an employee sales report.
+This query should return:
+-  the employeeID
+-  the employee's first and last name
+-  the name of each product
+-  and how many of that product they sold */
